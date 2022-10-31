6d5907459fd3c3a69556511176139e8995328599
mijalmaleh <mijalmaleh@gmail.com>
2022-10-31 17:17:07 -0300

    creacion de usuario, encriptacion de contraseña, validaciones en register. falta fechas

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,4 +1,15 @@
 const path = require ('path');
+const fs = require('fs');
+/*const multer = require("multer");*/
+const bcryptjs = require('bcryptjs');
+const usersJSON = fs.readFileSync(path.join(__dirname, "../data/users.json"), "utf-8");
+const usuarios = JSON.parse(usersJSON);
+const { validationResult } = require('express-validator')
+
+
+
+
+
 
 const mainController = {
     index: (req,res)=>{
@@ -8,9 +19,62 @@ const mainController = {
     login: (req,res)=>{
        return res.render('login');
     },
-    
-    register: (req,res)=>{
+    getRegister:(req,res)=>{
         return res.render('register');
+     },
+
+    register: (req,res)=>{
+
+        let errores = validationResult(req);
+        if (errores.isEmpty()) {
+            const usuarioRegistrado = usuarios.find(thisUser => thisUser.email === req.body.email);
+            if (usuarioRegistrado) {
+                res.render('register', {
+                    error: [
+                        { msg: 'Este mail ya esta registrado' }]
+                })
+            } else {
+
+                const nuevoUsuario = {
+                    id: Date.now(),
+                    email: req.body.email,
+                    nombre: req.body.nombre,
+                    apellido: req.body.apellido,
+                    /*profilePhoto: "./profilePhotos/" + req.file.filename,*/
+                    contrasenia: req.body.clave,
+                    confirmacionDeContrasenia: req.body.claveControl,
+                    fechaNacimiento:req.body.fechaNac,
+                    fechaNacimientoHebrea:req.body.fechaNacheb,
+                    pais:req.body.pais,
+                    ciudad:req.body.ciudad,
+                    barrio:req.body.barrio,
+                    //encriptar contraseña
+                    contrasenia: bcryptjs.hashSync(req.body.clave, 10),
+                    confirmacionDeContrasenia: bcryptjs.hashSync(req.body.claveControl, 10),
+                };
+
+                if (req.body.clave !== req.body.claveControl) {
+                    res.render('register', {
+                        error: [
+                            { msg: 'Las contraseñas no coinciden' }]
+                    });
+                } else {
+
+                    usuarios.push(nuevoUsuario);
+
+                    const usuariosActualizadosJSON = JSON.stringify(usuarios);
+
+                    fs.writeFileSync(path.join(__dirname, "../data/users.json"), usuariosActualizadosJSON, "utf-8");
+                    console.log(usuarios);
+                    res.redirect('/login');
+                }
+             }
+            } else {
+            res.render('register', {
+                error: errores.array(),
+                datosIngresados: req.body
+            });
+         }
     },
     
     error: (req,res)=>{
--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -1,14 +1,25 @@
 const express = require ('express');
 const mainController = require ('../controllers/mainController');
 const router = express.Router();
+const { body } = require('express-validator');
 
 
+const registerValidaciones =[
+    body('nombre').notEmpty().withMessage('Este campo es obligatorio'), 
+    body('apellido').notEmpty().withMessage('Este campo es obligatorio'),
+    body('email').notEmpty().withMessage('Este campo es obligatorio').bail()
+        .isEmail().withMessage('Por favor ingrese un email válido'),
+    body('clave').notEmpty().withMessage('Este campo es obligatorio').bail()
+        .isLength({ min:4  }).withMessage('La contraseña debe ser mas larga'),  
+          body('claveControl').notEmpty().withMessage('Este campo es obligatorio').bail()
+];
 router.get('/',mainController.index);
 
 router.get('/login',mainController.login);
 
-router.get('/Register',mainController.register)
+router.get('/Register',mainController.getRegister);
+router.post('/Register',registerValidaciones,mainController.register);
 
-router.get('/error',mainController.error)
+router.get('/error',mainController.error);
 
 module.exports = router;  
