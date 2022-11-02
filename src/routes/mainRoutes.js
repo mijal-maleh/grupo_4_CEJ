a91f91a82ced3c74861b2dd918daf9e436700eb9
mijalmaleh <mijalmaleh@gmail.com>
2022-11-01 23:50:45 -0300

    proceso de login

--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -5,21 +5,36 @@ const { body } = require('express-validator');
 
 
 const registerValidaciones =[
-    body('nombre').notEmpty().withMessage('Este campo es obligatorio'), 
+     body('nombre').notEmpty().withMessage('Este campo es obligatorio'), 
     body('apellido').notEmpty().withMessage('Este campo es obligatorio'),
     body('email').notEmpty().withMessage('Este campo es obligatorio').bail()
         .isEmail().withMessage('Por favor ingrese un email válido'),
-    body('clave').notEmpty().withMessage('Este campo es obligatorio').bail()
+    body('contrasenia').notEmpty().withMessage('Este campo es obligatorio').bail()
         .isLength({ min:4  }).withMessage('La contraseña debe ser mas larga'),  
-          body('claveControl').notEmpty().withMessage('Este campo es obligatorio').bail()
+          body('confirmacionDeContrasenia').notEmpty().withMessage('Este campo es obligatorio').bail() 
+          
 ];
+
+const loginValidaciones =[
+    
+    body('email').notEmpty().withMessage('Ingrese su correo electrónico').bail()
+        .isEmail().withMessage('Por favor ingrese un email válido'),
+    body('contrasenia').notEmpty().withMessage('Ingrese su contraseña').bail()
+        .isLength({ min:4  }).withMessage('La contraseña no es correcta'),
+]; 
+
+
+
+
 router.get('/',mainController.index);
 
-router.get('/login',mainController.login);
+router.get('/login',mainController.getLogin);
+router.post('/login',loginValidaciones,mainController.login);
 
 router.get('/Register',mainController.getRegister);
 router.post('/Register',registerValidaciones,mainController.register);
 
 router.get('/error',mainController.error);
+router.get('/logout', mainController.logout);
 
 module.exports = router;  
